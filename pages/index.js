8231ba5b71238b27b909b8037cd810b19adda928
Stefan Nikolov <stefan.nikolov@abv.bg>
2022-07-20 15:26:08 +0300

    add slide animation to carousel

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home(props) {
 
   return (
     <Layout>
-      <Carousel className={classes.mt1} animation="fade" height={200}>
+      <Carousel className={classes.mt1} animation="slide">
         {featuredProducts.map((product) => (
           <NextLink
             key={product._id}
